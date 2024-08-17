176fa828b7544e49efe9688f53bdbc0ce8a96778
CSAN7690 <christophersanchez@pursuit.org>
2024-08-17 17:43:27 -0400

    Setup App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import NewSightingPage from './components/NewSightingPage';
 
 const App = () => {
   return (
     <Router>
-
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/new-sighting" element={<NewSightingPage />} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
